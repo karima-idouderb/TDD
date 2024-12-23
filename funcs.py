06cba5aadae7f87235fc669570570bca1dfd6931
karima-idouderb <karima.idouderb@etu.sorbonne-universite.fr>
2024-12-23 19:58:02 +0100

    Trier la liste en ordre décroissant

--- funcs.py
+++ funcs.py
@@ -1,2 +1,4 @@
 def trois_grde_val(liste):
-    pass
+    # Fonction qui renvoie les 3 plus grandes valeurs d'une liste d'entiers.
+    # Si la liste a moins de 3 éléments, la fonction renvoie tous les éléments triés.
+    return sorted(liste, reverse=True)
