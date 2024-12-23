7a93fc13d96b7883cc5d9cd5d520f4e70c935858
karima-idouderb <karima.idouderb@etu.sorbonne-universite.fr>
2024-12-23 19:50:38 +0100

    Ajout de tests

--- tests.py
+++ tests.py
@@ -3,7 +3,11 @@ import unittest
 
 class TestFuncs(unittest.TestCase):
     def test_trois_grde_val(self):
-        self.assertEqual(funcs.trois_grde_val([1, 2, 3, 4, 5]), [3,4,5])
+        self.assertEqual(funcs.trois_grde_val([1, 2, 3, 4, 5]), [5, 4, 3])
+        self.assertEqual(funcs.trois_grde_val([6, -1, 0, 9, 5]), [9, 5, 6])
+        self.assertEqual(funcs.trois_grde_val([7, 7, 7]), [7, 7, 7])
+        self.assertEqual(funcs.trois_grde_val([150, 800]), [150, 800])
+        self.assertEqual(funcs.trois_grde_val([]), [])
 
 if __name__ == '__main__':
     unittest.main()
