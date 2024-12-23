0dc39e2a467267c86bd3d268e45e7405a6d7d9db
karima-idouderb <karima.idouderb@etu.sorbonne-universite.fr>
2024-12-23 20:03:11 +0100

    Modification de la fonction pour qu'elle renvoie les 3 premiers éléments de la liste triée et ajustement des tests

--- funcs.py
+++ funcs.py
@@ -1,4 +1,4 @@
 def trois_grde_val(liste):
     # Fonction qui renvoie les 3 plus grandes valeurs d'une liste d'entiers.
     # Si la liste a moins de 3 éléments, la fonction renvoie tous les éléments triés.
-    return sorted(liste, reverse=True)
+    return sorted(liste, reverse=True)[:3]
--- tests.py
+++ tests.py
@@ -4,9 +4,9 @@ import unittest
 class TestFuncs(unittest.TestCase):
     def test_trois_grde_val(self):
         self.assertEqual(funcs.trois_grde_val([1, 2, 3, 4, 5]), [5, 4, 3])
-        self.assertEqual(funcs.trois_grde_val([6, -1, 0, 9, 5]), [9, 5, 6])
+        self.assertEqual(funcs.trois_grde_val([6, -1, 0, 9, 5]), [9, 6, 5])
         self.assertEqual(funcs.trois_grde_val([7, 7, 7]), [7, 7, 7])
-        self.assertEqual(funcs.trois_grde_val([150, 800]), [150, 800])
+        self.assertEqual(funcs.trois_grde_val([150, 800]), [800, 150])
         self.assertEqual(funcs.trois_grde_val([]), [])
 
 if __name__ == '__main__':
