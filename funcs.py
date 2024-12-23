7df98a41a9052f8adcbea6270fa0a68633859555
karima-idouderb <karima.idouderb@etu.sorbonne-universite.fr>
2024-12-23 20:13:17 +0100

    Rajout d'une condition pour les listes de taille inférieure à 3 afin de renvoyer tous les éléments de la liste triée dans l'ordre décroissant

--- funcs.py
+++ funcs.py
@@ -1,4 +1,6 @@
 def trois_grde_val(liste):
     # Fonction qui renvoie les 3 plus grandes valeurs d'une liste d'entiers.
     # Si la liste a moins de 3 éléments, la fonction renvoie tous les éléments triés.
+    if len(liste) < 3:
+        return sorted(liste, reverse=True)
     return sorted(liste, reverse=True)[:3]
